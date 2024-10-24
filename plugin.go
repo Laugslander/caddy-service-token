4f9200f7045c13300e21cd2d8e87922c2970a151
Robin Laugs <robin.laugs@philips.com>
2024-10-24 14:17:39 +0000

    Initial commit

---
+++ plugin.go
@@ -0,0 +1,32 @@
+package service_token
+
+import (
+	"fmt"
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"net/http"
+)
+
+func init() {
+	caddy.RegisterModule(ServiceToken{})
+}
+
+type ServiceToken struct{}
+
+func (ServiceToken) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.handlers.service-token",
+		New: func() caddy.Module { return new(ServiceToken) },
+	}
+}
+
+func (h ServiceToken) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	fmt.Print(w, "Hello from my plugin!")
+
+	return next.ServeHTTP(w, r)
+}
+
+var (
+	_ caddy.Module                = (*ServiceToken)(nil)
+	_ caddyhttp.MiddlewareHandler = (*ServiceToken)(nil)
+)
